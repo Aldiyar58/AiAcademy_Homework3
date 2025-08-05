69cb17e5baef40668273f5bfec829014e96d316c
Алдияр Уалиев <u.aldiyarkz@gmail.com>
2025-08-06 02:05:09 +0500

    check col work

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,8 @@ col1, col2 = st.columns(2)
 with col1:
     fig1 = px.histogram(df, x="embarked", color="survived", barmode="group", title="Распределение выживших по городам")
     st.plotly_chart(fig1, use_container_width=True)
+    fig2 = px.scatter(df, x="age", y="sibsp", color="survived", title="Возраст vs количество родственников")
+    st.plotly_chart(fig2, use_container_width=True)
 with col2:
     fig2 = px.scatter(df, x="age", y="sibsp", color="survived", title="Возраст vs количество родственников")
     st.plotly_chart(fig2, use_container_width=True)
-
